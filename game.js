99309b6d34e91e72976631814cb3113d9e7a3279
mrmorkeberg <mrmorkeberg@gmail.com>
2021-06-21 18:58:14 +0000

    adde the first code to js and two audio files to the folder audio

---
+++ game.js
@@ -0,0 +1,9 @@
+/* All 4 color of blocks */
+let redBlock = document.querySelector('.redBlock');
+let greenBlock = document.querySelector('.greenBlock'); 
+let blueBlock = document.querySelector('.blueBlock');
+let orangeBlock = document.querySelector('.orangeBlock');
+
+// Unmute both audio files
+document.getElementById('correct').muted = false;
+document.getElementById('wrong').muted = false; 
