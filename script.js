ffaf1cdf2372a06946312ac01a0c838c7a328772
mrmorkeberg <mrmorkeberg@gmail.com>
2021-06-24 09:47:39 +0000

    changed the name of game.js to script.js and moved the the link to the script down to line 57 in index.html

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 //Start the game
-function startGame() {
+function startGame() 
+{
     document.getElementById('instructions').style.display = 'none';
     document.getElementById('game').style.display = 'block';
     setTimeout(() => {
         startBlinking();
     }, 1000);
-}
-//start blinking of the block which are in sequence
-const startBlinking = async() => {
-    allowClick = false;
-    for(let block of sequence) {
-        await blink(block);
-    }
-    allowClick = true;
 };
 /* All 4 color of blocks */
 let redBlock = document.querySelector('.redBlock');
@@ -21,28 +14,30 @@ let blueBlock = document.querySelector('.blueBlock');
 let orangeBlock = document.querySelector('.orangeBlock');
 
 // Unmute both audio files
-document.getElementById('correct').muted = false;
+document.getElementById("correct").muted = false;
 document.getElementById('wrong').muted = false; 
 
 /*Generate one random block during the gameplay */
 const getRandomBlock = () => {
-    const allBlocks = [redBlock, greenBlock, orangeBlock, blueBlock];
-    return allBlocks [parseInt(Math.random() * allBlocks.length)];
-}
+    const allBlocks = [redBlock,greenBlock,orangeBlock,blueBlock];
+    return allBlocks[parseInt(Math.random() * allBlocks.length)];
+};
+
 // Get the random generated block through function call
 let sequence = [getRandomBlock()];
 let sequenceToGuess = [...sequence];
 
 const blink = block => {
-    return new Promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         //unmute the audio sound
         document.getElementById('correct').muted = false;
         //add the active class on the block to identify which is generated and is blinking
         block.classList.add('active');
         //Play the audio on blinking block
-        let correct = document.getElementById('correct'); correct.play();
+        let correct = document.getElementById('correct'); 
+        correct.play();
         //After 750ms dely, remove the active class from the blinking block
-        setTimeout (() => {
+        setTimeout(() => {
             document.getElementById('correct').muted = true;
             document.getElementById('correct').pause();
             document.getElementById('correct').currentTime = 0;
@@ -59,16 +54,18 @@ let allowClick = false;
 const blockClicked = blockClicked => {
     if(!allowClick) return;
     let expectedBlock = sequenceToGuess.shift();
-    if(expectedBlock === blockClicked) {
+    if(expectedBlock === blockClicked) 
+    {
         document.getElementById('correct').muted = false;
         let correct = document.getElementById('correct');
         correct.play();
         setTimeout(() => {
             document.getElementById('correct').muted = true;
-            document.getElementById('correct').puase();
+            document.getElementById('correct').pause();
             document.getElementById('correct').currentTime = 0;
         }, 500);
-        if(sequenceToGuess.length === 0) {
+        if(sequenceToGuess.length === 0) 
+        {
             let currentLevel = parseInt(document.getElementById('level').innerHTML);
             currentLevel = currentLevel+1;
             document.getElementById('level').innerHTML = currentLevel;
@@ -84,8 +81,9 @@ const blockClicked = blockClicked => {
         let wrong = document.getElementById('wrong');
         wrong.play();
         let bLength = document.querySelectorAll('.panel');
-        for (let b = 0; b < bLength.length; b++) {
-            bLength[b].getElementsByClassName.pointerEvents = 'nome';
+        for (let b = 0; b < bLength.length; b++) 
+        {
+            bLength[b].style.pointerEvents = 'nome';
         }
         //Get the current level
         let currentLevel = parseInt(document.getElementById('level').innerHTML);
@@ -96,7 +94,18 @@ const blockClicked = blockClicked => {
     }
 };
 
+//Start blinking of the block which are in sequense
+const startBlinking = async() => {
+    allowClick = false;
+    for (let block of sequence) 
+    {
+        await blink(block);
+    }
+    allowClick = true;
+}
+
 //Restart the game function
-function restartGame() {
+function restartGame() 
+{
     window.location.reload();
 }
